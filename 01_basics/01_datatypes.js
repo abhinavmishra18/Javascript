df066baa6bcf2a122a67b1257de87f2cf0832b2a
Abhinav Mishra <abhinav.2725@gmail.com>
2023-10-22 20:34:51 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version 
+
+ /* Data Types 
+
+    --> Primitive 
+
+    - number ==> 2 to power 53
+    - bigint 
+    - string ==> ""
+    - boolean ==> true/false
+    - null ==> standalone value (means empty)
+    - undefined ==> not defined 
+    - symbol ==> unique
+
+    --> Non Primitive 
+
+    - object  
+ */
+
+    console.log(typeof undefined); // undefined 
+    console.log(typeof null); // object
