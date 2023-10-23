2edbe8703494cb12b514a503ce98cee7c6a44673
Abhinav Mishra <abhinav.2725@gmail.com>
2023-10-23 21:08:37 +0000

    Nums&Maths Done

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,16 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+// Prefer MDN Docs for more
+
+// ********************** Maths ********************
+
+console.log(Math); // Math --> Object (check in browser)
+
+// Prefer MDN Docs for more
