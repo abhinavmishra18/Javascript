8eb38cc259e51b21044a300f20807c03023f8d83
Abhinav Mishra <abhinav.2725@gmail.com>
2023-10-23 20:38:36 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,13 @@
+const name = "Abhinav"
+const repoCount = 50
+
+console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Abhinav')
+
+console.log(gameName[0]); // This is a key and will Access a value that will be 'h'
+
+// Prefer MDN Docs for String methods
+
